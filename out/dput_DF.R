3e7b016385947d570446b047023c26831533fe4e
AlexashaSova <sovyshka07@gmail.com>
2023-08-01 03:02:22 +0300

    Datatypes and test-1 completed

---
+++ out/dput_DF.R
@@ -0,0 +1,2 @@
+structure(list(col_A = c(1L, 3L, 5L), colB = c(2L, 4L, 6L)), class = "data.frame", row.names = c(NA, 
+-3L))
---
+++ out/dput_children1.R
@@ -0,0 +1,3 @@
+structure(list(name = c("Sarah", "Daniel", "Connor"), weight = c(5.6, 
+3.5, 10.4), height = c(91, 80, 101)), class = "data.frame", row.names = c(NA, 
+-3L))
